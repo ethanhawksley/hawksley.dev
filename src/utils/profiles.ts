415994464a0cfc98a50617c848aeef151fd0dfad
Ethan Hawksley <noreply@hawksley.dev>
2026-09-15 16:08:12 +0100

    feat: shuffle order of profiles

--- src/utils/profiles.ts
+++ src/utils/profiles.ts
@@ -1,6 +1,6 @@
 export const profileSections = [
   {
-    title: 'Development',
+    title: 'Code',
     links: [
       {
         name: 'GitHub',
@@ -23,37 +23,12 @@ export const profileSections = [
         url: 'https://ethanhawksley.itch.io',
       },
       {
-        name: 'Stack Overflow',
-        url: 'https://stackoverflow.com/users/27157731/ethan-hawksley',
+        name: 'Hack Club Scrapbook',
+        url: 'https://scrapbook.hackclub.com/ethanhawksley',
       },
       {
         name: 'LeetCode',
         url: 'https://leetcode.com/u/ethanhawksley/',
-      },
-    ],
-  },
-  {
-    title: 'Professional',
-    links: [
-      {
-        name: 'LinkedIn',
-        url: 'https://www.linkedin.com/in/ethanhawksley',
-      },
-      {
-        name: 'ORCID',
-        url: 'https://orcid.org/0009-0005-6663-0640',
-      },
-      {
-        name: 'Gravatar',
-        url: 'https://gravatar.com/ethanhawksley',
-      },
-      {
-        name: 'Bold.pro',
-        url: 'https://bold.pro/my/ethanhawksley',
-      },
-      {
-        name: 'Wikidata',
-        url: 'https://www.wikidata.org/wiki/Q141459330',
       },
     ],
   },
@@ -80,9 +55,26 @@ export const profileSections = [
         name: 'Hacker News',
         url: 'https://news.ycombinator.com/user?id=ethanhawksley',
       },
+    ],
+  },
+  {
+    title: 'Professional',
+    links: [
       {
-        name: 'Hack Club Scrapbook',
-        url: 'https://scrapbook.hackclub.com/ethanhawksley',
+        name: 'LinkedIn',
+        url: 'https://www.linkedin.com/in/ethanhawksley',
+      },
+      {
+        name: 'ORCID',
+        url: 'https://orcid.org/0009-0005-6663-0640',
+      },
+      {
+        name: 'Wikidata',
+        url: 'https://www.wikidata.org/wiki/Q141459330',
+      },
+      {
+        name: 'Gravatar',
+        url: 'https://gravatar.com/ethanhawksley',
       },
     ],
   },
@@ -98,7 +90,7 @@ export const profileSections = [
         url: 'https://mastodon.social/@ethanhawksley',
       },
       {
-        name: 'X (Twitter)',
+        name: 'Twitter (X)',
         url: 'https://x.com/Ethan_Hawksley',
       },
       {
@@ -106,29 +98,8 @@ export const profileSections = [
         url: 'https://www.youtube.com/@Ethan-Hawksley',
       },
       {
-        name: 'Reddit',
-        url: 'https://www.reddit.com/user/ethanhawksley/',
-      },
-      {
         name: 'Instagram',
         url: 'https://www.instagram.com/ethan.hawksley/',
-      },
-    ],
-  },
-  {
-    title: 'Security',
-    links: [
-      {
-        name: 'TryHackMe',
-        url: 'https://tryhackme.com/p/ethanhawksley',
-      },
-      {
-        name: 'Hack The Box',
-        url: 'https://app.hackthebox.com/public/users/3608157',
-      },
-      {
-        name: 'HackerOne',
-        url: 'https://hackerone.com/ethanhawksley',
       },
     ],
   },
