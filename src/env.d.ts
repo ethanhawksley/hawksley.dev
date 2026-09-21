db0f18e5de4a2f3cf5032c5a4b7ba4da8851996d
Ethan Hawksley <noreply@hawksley.dev>
2026-09-21 17:04:27 +0100

    feat: switch back to MPA

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,15 +1,6 @@
-interface Navigator {
-  userAgentData?: {
-    brands: { brand: string }[];
-  };
-}
-
 interface Window {
   __getTheme: () => 'light' | 'dark';
   __setTheme: (theme: 'light' | 'dark', save?: boolean, doc?: Document) => void;
-  __themeTransition?:
-    | ((updateCallback: () => Promise<void> | void) => ViewTransition)
-    | null;
   yes: any;
   no: any;
 }
