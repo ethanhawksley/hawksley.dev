d88fea98d41aac3e27bd1632de58a8354cfd3d77
Ethan Hawksley <noreply@hawksley.dev>
2026-09-21 17:47:58 +0100

    refactor: use typescript in Misc.astro instead of javascript

--- src/env.d.ts
+++ src/env.d.ts
@@ -3,4 +3,17 @@ interface Window {
   __setTheme: (theme: 'light' | 'dark', save?: boolean, doc?: Document) => void;
   yes: any;
   no: any;
+
+  callPhantom?: unknown;
+  _phantom?: unknown;
+  phantom?: unknown;
+  __nightmare?: unknown;
+}
+
+interface Document {
+  prerendering?: boolean;
+
+  __selenium_unwrapped?: unknown;
+  __webdriver_evaluate?: unknown;
+  __driver_evaluate?: unknown;
 }
